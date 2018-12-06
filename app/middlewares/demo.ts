9409670cfa2cbe5d60f606b912c795681e841faf
Big Mogician <sunaoran@youzan.com>
2018-12-06 17:48:58 +0800

    update demo

--- app/middlewares/demo.ts
+++ app/middlewares/demo.ts
@@ -1,8 +1,8 @@
-import { injectScope } from "astroboy.ts";
+import { injectScope, ENV, Context } from "astroboy.ts";
 
 export = () => injectScope(async ({ injector, configs, ctx, next }) => {
-  // console.log(injector);
-  // console.log(configs);
+  // console.log(injector.get(Context).ctx);
+  // console.log(configs.get(ENV).showTrace);
   await next();
   // console.log("fuck");
 });
