8eb4cba1e8bce587fa68e008148c49e00b12b6f0
Big Mogician <sunaoran@youzan.com>
2018-12-10 23:54:19 +0800

    update demo

---
+++ app/routers/sec/sdc/sdads/ads.js
@@ -0,0 +1,5 @@
+// [astroboy.ts] 自动生成的代码
+
+const CTOR = require('/Users/bigmogician/Documents/github.com/ws-node/demo.astroboy.ts/app/controllers/sec/sdc/sdads/ads');
+const { buildRouter } = require("astroboy.ts");
+module.exports = buildRouter(CTOR, "sec.sdc.sdads.ads", "/v1");
