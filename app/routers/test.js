58fea529251dd1efd60d7acc132cfbb657874411
Big Mogician <sunaoran@youzan.com>
2018-12-10 23:25:13 +0800

    update demo

--- app/routers/test.js
+++
@@ -1,5 +0,0 @@
-// [astroboy.ts] 自动生成的代码
-
-const CTOR = require('/Users/bigmogician/Documents/github.com/ws-node/demo.astroboy.ts/app/controllers/test');
-const { buildRouter } = require("astroboy.ts");
-module.exports = buildRouter(CTOR, "test", "/v1");
