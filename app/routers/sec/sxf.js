c3e4acc3946b8474f84e1329e5fe5a8fab4387c4
Big Mogician <sunaoran@youzan.com>
2018-12-10 23:15:24 +0800

    update demo

--- app/routers/sec/sxf.js
+++ app/routers/sec/sxf.js
@@ -1,4 +1,5 @@
-// [astroboy.ts]自动生成的代码
+// [astroboy.ts] 自动生成的代码
+
 const CTOR = require('/Users/bigmogician/Documents/github.com/ws-node/demo.astroboy.ts/app/controllers/sec/sxf');
 const { buildRouter } = require("astroboy.ts");
 module.exports = buildRouter(CTOR, "sec.sxf", "/v1");
--- app/routers/test.js
+++ app/routers/test.js
@@ -1,4 +1,5 @@
-// [astroboy.ts]自动生成的代码
+// [astroboy.ts] 自动生成的代码
+
 const CTOR = require('/Users/bigmogician/Documents/github.com/ws-node/demo.astroboy.ts/app/controllers/test');
 const { buildRouter } = require("astroboy.ts");
 module.exports = buildRouter(CTOR, "test", "/v1");
