97d43e83d4d0570d57e2e4d5052262f27d1b0f51
Big Mogician <sunaoran@youzan.com>
2019-02-27 16:40:32 +0800

    update dmeo

--- app/services/fac.ts
+++ app/services/fac.ts
@@ -1,14 +1,16 @@
-import { AstroboyContext, FactoryContext } from "astroboy.ts";
+import { AstroboyContext, InjectService } from "astroboy.ts";
 
 abstract class FactoryService {
   public data: number;
   public context: AstroboyContext;
-  public static factory({ injector }: FactoryContext): FactoryService {
-    return {
+
+  public static factory = [
+    [AstroboyContext],
+    (context: AstroboyContext) => ({
       data: 123,
-      context: injector.get(AstroboyContext)
-    };
-  }
+      context
+    })
+  ];
 }
 
 export = FactoryService;
